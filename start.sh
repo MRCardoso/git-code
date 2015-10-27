41557d4a2151f75eea8f07719fa745dd2447fcd5
Marlon Cardozo <cardozinhos.mrc@gmail.com>
2015-10-27 13:13:18 -0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#git init
+create a README.md file
+
+vim touch README.md
+add README.md
+
+git add README.md
+commit file
+
+git commit -m "my first commit"
+create a remote repository
+
+git remote add origin git@github.com:MRCardoso/git-code.git
+push the file for repository in github
+
+git push origin master
